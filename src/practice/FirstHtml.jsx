bd7295cfb77b5a6d6110494533c3f55f162c9497
unknown <javaidminhal1@gmail.com>
2024-02-10 22:30:29 +0000

    react project setup done

---
+++ src/practice/FirstHtml.jsx
@@ -0,0 +1,13 @@
+
+function FirstHtml() {
+    return (
+      <>
+          <button className="tailwind-bg-pink-600 tailwind-text-sm tailwind-text-white">
+            Submit
+          </button>
+      </>
+    )
+  }
+  
+  export default FirstHtml
+  
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+prefix: 'tailwind-',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
