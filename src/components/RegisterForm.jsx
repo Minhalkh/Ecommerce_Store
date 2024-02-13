5eae360b25b1de60f0142c6fd00d0c70cd0bb60c
unknown <javaidminhal1@gmail.com>
2024-02-13 00:44:22 +0000

    started frontend design

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,1 +1,18 @@
 // This is RegisterForm
+// This is RegisterForm
+
+function RegistrationForm() {
+    return (
+      <>
+      {/* write code for the registration form */}
+      <div className="registration-form tailwind-flex tailwind-flex-col tailwind-ml-2">
+        <h1>Register</h1>
+        <div className="box tailwind-border-2 tailwind-h-96 tailwind-w-[400px] tailwind-rounded-3xl tailwind-border-black">
+            
+        </div>
+      </div>
+      </>
+    )
+}
+
+export default RegistrationForm
