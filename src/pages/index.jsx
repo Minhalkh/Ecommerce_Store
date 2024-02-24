f5e876efcc2b9cc17190d3b7ccfba263d020dc05
unknown <javaidminhal1@gmail.com>
2024-02-24 23:35:03 +0000

    created about route and update navbar

--- src/pages/index.jsx
+++
@@ -1,6 +0,0 @@
-import Home from '../pages/HomePage'
-import Login from '../pages/LoginPage'
-import Register from '../pages/RegisterPage'
-import NotFound from '../components/NotFound'
-
-export { Home, Login, Register, NotFound }
