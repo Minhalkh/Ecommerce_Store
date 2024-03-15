42b4d15920fd2374745cfd86347edb47768486e0
unknown <javaidminhal1@gmail.com>
2024-03-15 21:35:24 +0000

    added new div in about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -3,6 +3,9 @@ function AboutPage(){
     return(
         <>
         <div>About Page------ Lorem ipsum dolor sit amet consectetur adipisicing elit. At quod harum veritatis enim laborum nostrum odio sequi, iure reprehenderit similique soluta commodi error. Adipisci maxime accusamus sit incidunt porro.</div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum fugiat quibusdam ex numquam exercitationem laboriosam, quo asperiores quae distinctio praesentium.
+        </div>
         </>
     )
 }
