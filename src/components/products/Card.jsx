226cdbede5c92502a30706d8875e97e59b517036
unknown <javaidminhal1@gmail.com>
2024-03-18 21:41:16 +0000

    add cards and products

--- src/components/products/Card.jsx
+++ src/components/products/Card.jsx
@@ -1,12 +1,38 @@
-// this is card
+// this is Card.jsx
 
 function Card(props) {
-    const {img , title} = props;
+    const { product } = props;
     return (
-        <div>
-            <div>
-                <img src={img} alt="watches" className="" />
-                <p>{title}</p>
+        <div className="tl-h-72 hover:tl-shadow-md hover:tl-shadow-slate-700 tl-rounded-md tl-cursor-pointer">
+            <img
+                src={product.img}
+                alt="earing"
+                className="tl-h-44 tl-w-60 tl-object-cover tl-rounded-t-lg"
+            />
+            <div className="tl-flex tl-flex-col tl-bg-white tl-border tl-border-slate-300 tl-h-28 tl-p-2 tl-rounded-b-lg">
+                <div className="tl-flex tl-justify-between">
+                    <div className="tl-flex">
+                        <p className="tl-font-semibold tl-text-2xl tl-flex tl-justify-center">
+                            {product.title}
+                        </p>
+                    </div>
+                    {/* conditional rendering */}
+                    {!product.favourite && (
+                        <i class="fa fa-heart-o tl-pt-2 tl-pr-1" />
+                    )}
+                    {product.favourite && (
+                        <i class="fa fa-heart tl-pt-2 tl-pr-1" />
+                    )}
+                </div>
+                <span className="tl-text-md">
+                    Rs. {product.price}
+                </span>
+                <p className="tl-text-sm tl-w-52 tl-truncate">
+                    {product.description}
+                </p>
+                <p className="tl-text-sm tl-w-52 tl-truncate">
+                    {product.location}
+                </p>
             </div>
         </div>
     )
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,4 +1,4 @@
-//This is products page
+// This is products page
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Filteration from "../components/products/Filteration";
@@ -8,11 +8,11 @@ function ProductsPage(){
     return(
         <div className="main-app tl-flex tl-flex-col tl-items-center tl-w-full tl-top-0 tl-absolute tl-bg-white">
             <Navbar />
-            <div className="tl-flex tl-w-full tl-h-[100vh]">
-            <div className="tl-w-1/4 tl-bg-red-400">
+            <div className="tl-flex tl-w-full">
+            <div className="tl-w-1/5 tl-bg-red-400">
                 <Filteration />
             </div>
-            <div className="tl-w-3/4 tl-bg-green-300">
+            <div className="tl-w-4/5 tl-bg-green-300">
                 <ProductsCardsSection />
             </div>
             </div>
