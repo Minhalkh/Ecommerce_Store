570655658ac7827851606152a9157b2b8e993974
unknown <javaidminhal1@gmail.com>
2024-04-27 17:47:07 +0100

    added some components in about page

---
+++ src/components/about/Icons.jsx
@@ -0,0 +1,12 @@
+// This is Icons
+function Icons(){
+    return(
+        <div className="Services-1 tl-mt-6 tl-flex">
+            <i class="fa fa-shopping-cart fa-5x" aria-hidden="true"></i>
+            <h3>Super Fast and Free Delivery</h3>
+            <i class="fa fa-truck fa-5x" aria-hidden="true"></i>
+            <h3>Super Fast and Free Delivery</h3>
+        </div>
+    );
+}
+export default Icons;
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,12 +1,23 @@
 //This is about page
 import Navbar from "../components/Navbar";
-import Crousal from "../components/home/Crousal"
+import Crousal from "../components/home/Crousal";
+import Paragraph from "../components/about/AboutParagraph";
+import CheckNowSection from "../components/home/CheckNowSection";
+import Icons from "../components/about/Icons";
+import Footer from "../components/Footer";
+
 function AboutPage() {
   return (
     <div className="main-app tl-flex tl-flex-col tl-items-center tl-w-full tl-top-0 tl-absolute tl-bg-white">
-    <Navbar/>
-      <Crousal/>
+      <Navbar />
+      <Crousal storeName="Milli E-Commerce" />
+      <CheckNowSection />
+      <Paragraph />
+      <Icons />
+      <Footer />
     </div>
   );
 }
+
 export default AboutPage;
+
